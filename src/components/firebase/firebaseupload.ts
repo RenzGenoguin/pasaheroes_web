ff1fd2aa7bb715acf0993704664cd07e5fe5a07b
renz <genoguinrenz@gmail.com>
2024-05-16 21:34:07 +0800

    chaange api firebase

--- src/components/firebase/firebaseupload.ts
+++ src/components/firebase/firebaseupload.ts
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { v4 } from "uuid";
 const firebaseConfig = {
-  apiKey: "AIzaSyAovtURtW21Ij_uTNVAu-BGRKZxr9esDMk",
-  authDomain: "imageuploadforthesises.firebaseapp.com",
-  projectId: "imageuploadforthesises",
-  storageBucket: "imageuploadforthesises.appspot.com",
-  messagingSenderId: "255663088615",
-  appId: "1:255663088615:web:5b31db93cd972fdeadd342",
+  apiKey: "AIzaSyCtt3Z5XZW2EakSl6RrwSXdwLRk3F6nNMg",
+  authDomain: "pasaheroes-f1831.firebaseapp.com",
+  projectId: "pasaheroes-f1831",
+  storageBucket: "pasaheroes-f1831.appspot.com",
+  messagingSenderId: "1061524938923",
+  appId: "1:1061524938923:web:d29b44fad2e37e85f557ee",
+  measurementId: "G-W5DBVGJS53"
 };
 
 // Initialize Firebase
